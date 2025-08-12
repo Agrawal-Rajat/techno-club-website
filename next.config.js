5a7c0f6625e2a217b1da40094b6a2844b26e9467
AgrawalRajat <agrawal21rajat@gmail.com>
2025-08-12 13:27:12 +0530

    fix config file

--- next.config.js
+++ next.config.js
@@ -1,24 +1,10 @@
 /** @type {import('next').NextConfig} */
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 const nextConfig = {
   images: {
     domains: ['localhost', 'res.cloudinary.com'],
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Add the mini-css-extract-plugin
-    if (!isServer) {
-      config.plugins.push(
-        new MiniCssExtractPlugin({
-          filename: 'static/css/[name].[contenthash:8].css',
-          chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
-        })
-      );
-    }
-    return config;
   },
 };
 
